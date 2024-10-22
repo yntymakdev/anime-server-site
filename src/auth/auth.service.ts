1289c041911dba3b2bd058fe632d7311c80fd108
yntymak <kubanychevyntymak21@gmail.com>
2024-10-22 22:45:39 +0600

    finish register in backend starting login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,48 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
 import { UserService } from 'src/user/user.service'
 import { AuthDto } from './dto/auth.dto'
+import { JwtService } from '@nestjs/jwt'
+import { verify } from 'argon2'
 
 @Injectable()
 export class AuthService {
 	constructor(
-		private prisma: PrismaService,
-		private userService: UserService
+		private userService: UserService,
+		private jwt: JwtService
 	) {}
 	async register(dto: AuthDto) {
 		const oldUser = await this.userService.getByEmail(dto.email)
 		if (oldUser)
 			throw new BadRequestException('Пользователь уже существует')
-		const user = this.userService.create(dto)
+		const user = await this.userService.create(dto)
+		const tokens = this.issueTokens(user.id)
 		return {
-			user
+			user,
+			...tokens
 		}
+	}
+	private issueTokens(userId: string) {
+		const data = { id: userId }
+		const accesToken = this.jwt.sign(data, {
+			expiresIn: '1h'
+		})
+		const refreshToken = this.jwt.sign(data, {
+			expiresIn: '7d'
+		})
+		return { accesToken, refreshToken }
+	}
+	private async validateUser(dto: AuthDto) {
+		const user = await this.userService.getByEmail(dto.email)
+		if (!user) throw new NotFoundException('Пользователь не найден')
+		const isValidPassword = await verify(user.password, dto.password)
+		if (!isValidPassword)
+			throw new UnauthorizedException('Неверный пароль ')
+		return user
 	}
 }
