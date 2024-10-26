3a8f6af5d316956d25c299369534c1f4ab21c271
yntymak <kubanychevyntymak21@gmail.com>
2024-10-26 17:24:43 +0600

    finish post and gets files for actors ^^

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
-import { GenreModule } from './genre/genre.module';
-import { ActorModule } from './actor/actor.module';
-import { ReviewModule } from './review/review.module';
-import { MovieModule } from './movie/movie.module';
-import { FileModule } from './file/file.module';
-import { StatisticModule } from './statistic/statistic.module';
+import { GenreModule } from './genre/genre.module'
+import { ActorModule } from './actor/actor.module'
+import { ReviewModule } from './review/review.module'
+import { MovieModule } from './movie/movie.module'
+import { FileModule } from './file/file.module'
+import { StatisticModule } from './statistic/statistic.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, GenreModule, ActorModule, ReviewModule, MovieModule, FileModule, StatisticModule]
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		GenreModule,
+		ActorModule,
+		ReviewModule,
+		MovieModule,
+		FileModule,
+		StatisticModule
+	]
 })
 export class AppModule {}
