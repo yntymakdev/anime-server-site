c17f42a0a9b37b86d5918ef711991c52dfd5811b
yntymak <kubanychevyntymak21@gmail.com>
2024-10-27 04:31:14 +0600

    finish and still ending payment service

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common'
+import { ConflictException, Injectable } from '@nestjs/common'
 import { Prisma } from '@prisma/client'
 import { PrismaService } from 'src/prisma.service'
 import { UserService } from 'src/user/user.service'
 import * as YooKassa from 'yookassa'
+import { PaymentDto } from './dto/payment.dto'
+import { AuthModuleOptions } from '@nestjs/passport'
+import { config } from 'process'
 const yooKassa = new YooKassa({
 	shopId: process.env.env['SHOP_ID'],
 	secretKey: process.env['PAYMENT_TOKEN']
@@ -14,5 +17,35 @@ export class PaymentService {
 		private userService: UserService
 	) {}
 
-	async checkout() {}
+	async checkout(dto: PaymentDto, userId: string) {
+		const user = await this.userService.getById(userId)
+		if (user.isHasPremium)
+			throw new ConflictException(
+				'У пользователя уже есть  активная  премиум-подписка.'
+			)
+		const order = await this.prisma.payment.create({
+			data: {
+				status: dto.status,
+				amount: dto.amount,
+				user: {
+					connect: {
+						id: userId
+					}
+				}
+			}
+		})
+
+		const payment = await yooKassa.createPayment({
+			amount: { value: dto.amount.toFixed(2), currency: 'KGS' },
+			payment_method_data: {
+				type: 'back_card'
+			},
+			confirmation: {
+				type: 'redirect',
+				return_url: `${process.env['APP_URL']}/thanks`
+			},
+			description: `Покупка платной подписки на премиум в Yume | Stories. Id платежа #${order.id}, Id пользователя  #${order.userId}`
+		})
+		return payment
+	}
 }
