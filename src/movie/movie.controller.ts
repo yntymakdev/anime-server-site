a482964e2ec344a7dbd10a2b2b7d0c04b289074c
yntymak <kubanychevyntymak21@gmail.com>
2024-10-27 00:12:42 +0600

    also fixed movie controller

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,17 +1,24 @@
 import {
 	Body,
 	Controller,
+	Delete,
 	Get,
 	HttpCode,
+	NotFoundException,
 	Param,
 	Post,
+	Put,
 	Query,
-	Search
+	Search,
+	UsePipes,
+	ValidationPipe
 } from '@nestjs/common'
 import { MovieService } from './movie.service'
 import { Auth } from 'src/auth/decorators/auth.decorator'
-import { generate } from 'rxjs'
+import { generate, NotFoundError } from 'rxjs'
 import { GenreService } from 'src/genre/genre.service'
+import { ValidationTypes } from 'class-validator'
+import { UpdateMovieDto } from './dto/update-movie.dto'
 
 @Controller('movies')
 export class MovieController {
@@ -31,20 +38,52 @@ export class MovieController {
 		return this.movieService.getMostPopular()
 	}
 
-	@Get('by-actor/:id')
+	@Get('by-actors/:id')
 	@HttpCode(200)
-	async getByActor(
-		@Param('id')
-		id: string
-	) {
+	async getByActor(@Param('id') id: string) {
 		return this.movieService.getByActor(id)
 	}
-
-	@Post('update-count-views')
+	@Post('by-genres')
 	@HttpCode(200)
-	async updateCountOPened(@Body('slug') slug: string[]) {
+	async getByGenres(@Body('genreIds') genreIds: string[]) {
+		return this.movieService.getByGenres(genreIds)
+	}
+
+	@Put('update-count-views')
+	@HttpCode(200)
+	async updateCountOPened(@Body('slug') slug: string) {
 		return this.movieService.updateCountViews(slug)
 	}
 
 	//? Query for admin
+
+	@Get('by-id/:id')
+	@Auth('admin')
+	async getById(@Param('id') id: string) {
+		return this.movieService.getById(id)
+	}
+
+	@Post()
+	@HttpCode(200)
+	@Auth('admin')
+	async create() {
+		return this.movieService.create()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async update(@Param('id') id: string, @Body() data: UpdateMovieDto) {
+		const updateMovie = this.movieService.update(id, data)
+		if (!updateMovie) throw new NotFoundException('Фильм не найден')
+		return updateMovie
+	}
+	@Delete(':id')
+	@Auth('admin')
+	async delete(@Param('id') id: string) {
+		const deletedMovie = await this.movieService.delete(id)
+		if (!deletedMovie) throw new NotFoundException('Фильм не найден')
+		return deletedMovie
+	}
 }
